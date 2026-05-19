8671d81520465e3203a0a2b66a2b205a5d9b5806
Sandip Patil <96sandypatil@gmail.com>
2026-05-19 17:31:11 +0530

    infra for dev stage prod added

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,40 @@
+variable "name" {
+  description = "VPC name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "intra_subnets" {
+  description = "Intra subnet CIDRs"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+}
