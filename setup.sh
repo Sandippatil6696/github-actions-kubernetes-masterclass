6d43cc58a3d5aac005c36bdebd15c92014a5df64
Sandip Patil <96sandypatil@gmail.com>
2026-05-19 19:31:33 +0530

    infra for dev stage prod added

--- setup.sh
+++ setup.sh
@@ -115,6 +115,37 @@ else
     sudo snap install kubectl --classic
 fi
 
+
+# ---------------------------------------------------
+# Helm Installation
+# ---------------------------------------------------
+
+echo "========================================="
+echo " Checking Helm"
+echo "========================================="
+
+if command -v helm &> /dev/null
+then
+    echo "Helm already installed. Skipping"
+else
+    echo "Installing Helm..."
+
+    sudo apt-get install curl gpg apt-transport-https -y
+
+    curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+
+    echo "deb [signed-by=/usr/share/keyrings/helm.gpg] \
+    https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | \
+    sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+    sudo apt-get update -y
+
+    sudo apt-get install helm -y
+fi
+
+
 # ---------------------------------------------------
 # AWS CLI v2 Installation
 # ---------------------------------------------------
@@ -155,6 +186,9 @@ else
     aws configure
 fi
 
+
+
+
 # ---------------------------------------------------
 # Versions
 # ---------------------------------------------------
@@ -168,6 +202,7 @@ docker-compose --version || true
 terraform version || true
 kubectl version --client || true
 aws --version || true
+helm version || true
 
 echo "========================================="
 echo " Installation Completed Successfully"
