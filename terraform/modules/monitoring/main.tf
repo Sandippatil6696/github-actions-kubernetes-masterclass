a0ab904ffc423ec8f713e8e87cfa94579ec97e92
Sandip Patil <96sandypatil@gmail.com>
2026-05-31 10:35:07 +0530

    infra added

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,30 +1,30 @@
 #Loki Stack (Loki + Promtail)
-resource "helm_release" "loki" {
-  name             = "loki"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "loki-stack"
-  namespace        = var.namespace
-  create_namespace = true
+# resource "helm_release" "loki" {
+#   name             = "loki"
+#   repository       = "https://grafana-community.github.io/helm-charts"
+#   chart            = "loki"
+#   namespace        = var.namespace
+#   create_namespace = true
 
-  wait    = true
-  timeout = 1200
-  atomic  = true
-  wait_for_jobs = true
+#   wait    = true
+#   timeout = 1200
+#   atomic  = true
+#   wait_for_jobs = true
 
-  values = [yamlencode({
-    loki = {
-      enabled = true
-      persistence = {
-        enabled = true
-        size    = "2Gi"
-        storageClassName = "gp2"
-      }
-    }
-    promtail = {
-      enabled = true
-    }
-  })]
-}
+#   values = [yamlencode({
+#     loki = {
+#       enabled = true
+#       persistence = {
+#         enabled = true
+#         size    = "2Gi"
+#         storageClassName = "gp2"
+#       }
+#     }
+#     promtail = {
+#       enabled = true
+#     }
+#   })]
+# }
 
 #Kube Prometheus Stack (Prometheus + Alertmanager + Grafana)
 resource "helm_release" "kube_prometheus" {
@@ -36,33 +36,31 @@ resource "helm_release" "kube_prometheus" {
 
   wait    = true
   timeout = 1200
-  atomic  = true
-  wait_for_jobs = true
-
-  
+ 
 
   values = [yamlencode({
     grafana = {
       service = {
         type = "ClusterIP" # same as --set grafana.service.type=LoadBalancer
       }
-      sidecar = {
-      datasources = {
-        defaultDatasourceEnabled = true
-      }
-    }
-      additionalDataSources = [
-        {
-          name   = "Loki"
-          type   = "loki"
-          access = "proxy"
-          url    = "http://loki.${var.namespace}.svc.cluster.local:3100"
-          isDefault = false
-          jsonData = {
-            maxLines = 1000
-          }
-        }
-      ]
+      
+    #   sidecar = {
+    #   datasources = {
+    #     defaultDatasourceEnabled = true
+    #   }
+    # }
+      # additionalDataSources = [
+      #   {
+      #     name   = "Loki"
+      #     type   = "loki"
+      #     access = "proxy"
+      #     url    = "http://loki.${var.namespace}.svc.cluster.local:3100"
+      #     isDefault = false
+      #     jsonData = {
+      #       maxLines = 1000
+      #     }
+      #   }
+      # ]
 
      
 
@@ -72,21 +70,21 @@ resource "helm_release" "kube_prometheus" {
  
 }
 
-# OpenTelemetry Collector
-resource "helm_release" "otel_collector" {
-  name             = "otel-collector"
-  repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
-  chart            = "opentelemetry-collector"
-  namespace        = var.namespace
-  create_namespace = true
+# # OpenTelemetry Collector
+# resource "helm_release" "otel_collector" {
+#   name             = "otel-collector"
+#   repository       = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+#   chart            = "opentelemetry-collector"
+#   namespace        = var.namespace
+#   create_namespace = true
 
-  wait    = true
-  timeout = 600
+#   wait    = true
+#   timeout = 600
 
-  values = [file("${path.module}/otel-config.yml")]
+#   values = [file("${path.module}/otel-config.yml")]
 
-  depends_on = [
-    helm_release.loki,
-    helm_release.kube_prometheus,
-  ]
-}
+#   depends_on = [
+#     helm_release.loki,
+#     helm_release.kube_prometheus,
+#   ]
+# }
